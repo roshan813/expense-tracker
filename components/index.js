5f1bcd0b9627fabbb938802cb8aeec162722c878
roshan813 <112106995+roshan813@users.noreply.github.com>
2023-08-15 20:36:48 +0530

    Delete components directory

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Details } from './Details/details';
-export { default as Main } from './Main/Main';
-export { default as Snackbar } from './Snackbar/Snackbar';
-export { default as InfoCard } from './InfoCard';
